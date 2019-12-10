6a239f9bbdd7eeefdb40421ac8ba203a2d0d1fca
PetrusHahol <petius96@gmail.com>
2019-12-10 21:04:42 +0100

    Added set instead of array to the chrome memory

--- src/js/collect.js
+++ src/js/collect.js
@@ -9,7 +9,9 @@ chrome.extension.sendMessage({title: 'get_data'},
             console.log("Retrieved");
             let ids = obj.ids;
             for (let i = 0, l = items.length; i < l; i++) {
-                ids.push({ids: items[i].dataset.id, HasBeenUploadedYet: false});
+                if (!ids.includes(items[i].dataset.id)) {
+                    ids.push(items[i].dataset.id);
+                }
             }
             chrome.storage.sync.set({"ids": ids})
         })
@@ -26,7 +28,7 @@ chrome.storage.sync.get({"ids": []}, function (obj) {
     if (storage_value) {
         for (var i = 0, f = 0, l = items.length; i < l; i++) {
             for (var j = 0, st_l = storage_value.length; j < st_l; j++) {
-                if (storage_value[j].ids === items[i].dataset.id) {
+                if (storage_value[j] === items[i].dataset.id) {
                     f += 1;
                 }
             }
